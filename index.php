719eb011c2e2fc742098715ca6b179a65f78420b
tomaselis <tsabaliauskis@hotmail.co.uk>
2019-05-08 19:04:06 +0300

    petras kariuomene

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php
+$vardas = 'Petras Petraitis';
+$amzius =12;
+$statusas = 'dirba';
+
+
+if($amzius > 0 && $amzius < 18) {
+    print $vardas . ' Yra nepilnametis. ';
+    print $vardas . ' Netinkamas kariuomenei';
+}elseif($amzius >= 18 && $amzius < 65){
+    print $vardas . ' Yra pilnametis ';
+    if($amzius <= 26) {
+        if($statusas == 'dirba' || $statusas == 'nedirba'){
+            print $vardas . ' Tinkamas Tarnybai.';
+        }elseif($statusas == 'studijuoja. '){
+            print $vardas . ' Netinkamas kariuomenei';
+        }
+    }elseif($amzius >= 27){
+        print $vardas . ' Netinkamas Kariuomenei. ';
+    }
+   // print $vardas . ' Netinkamas kariuomenei';
+} elseif($amzius >= 65){
+    print $vardas . ' Yra Pensininkas ir ';
+    print $vardas . ' netinkamas kariuomenei';
+}
+elseif($amzius < 0){
+    print 'Klaida. Blogai nuordytas amzius';
+}
+?>
+
+
 
 
 <html>
